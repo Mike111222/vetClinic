c7934624f86f5ff9760106a74ddeeb1c688f492e
Mike111222 <Tipu870@gmail.com>
2023-08-16 16:02:58 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -11,3 +11,43 @@ CREATE TABLE animals(
   species           VARCHAR(250),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE owners (
+    id              SERIAL PRIMARY KEY,
+    full_name       VARCHAR,
+    age             INTEGER
+);
+
+CREATE TABLE species (
+    id              SERIAL PRIMARY KEY,
+    name            VARCHAR
+);    
+
+-- Step 1: Drop the primary key constraint (if exists)
+ALTER TABLE animals DROP CONSTRAINT IF EXISTS animals_pkey;
+
+-- Step 2: Add a new serial column and set it as primary key
+ALTER TABLE animals
+    ADD COLUMN new_id SERIAL PRIMARY KEY;
+
+-- Step 3: Update the new column with the values from the original 'id' column
+UPDATE animals
+SET new_id = id;
+
+-- Step 4: Drop the original 'id' column
+ALTER TABLE animals
+    DROP COLUMN id;
+
+-- Step 5: Rename the new column to 'id'
+ALTER TABLE animals
+    RENAME COLUMN new_id TO id;
+
+-- Step 6: Remove 'species' column
+ALTER TABLE animals
+    DROP COLUMN IF EXISTS species;
+
+-- Step 7: Add 'species_id' and 'owner_id' columns as foreign keys
+ALTER TABLE animals
+    ADD COLUMN species_id INTEGER REFERENCES species(id),
+    ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+    
